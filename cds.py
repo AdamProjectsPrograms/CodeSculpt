8107137a06ebda75ff1765f95efa4cfc93906783
AdamProjectsPrograms <channelpro964@gmail.com>
2024-05-15 18:59:11 +0300

    add

--- cds.py
+++ cds.py
@@ -8,5 +8,6 @@ tokensp = {
     "KEYWORD_ENAP_F": r"ENAP \@0",
     "KEYWORD_MPTL": r"MPTL \*[0-9][0-9][0-9][0-9] : \\_[a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z]\...",
     "KEYWORK_CSPT": r"CSPT \*[0-9][0-9][0-9][0-9] : \\_[a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z]\.TCF",
-    "INT": r"INT [0-9][0-9]h"
+    "INT": r"INT [0-9][0-9]h",
+    
 }
