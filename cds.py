8802f9206ecd68137d1409ef210131ce335d4f1a
AdamProjectsPrograms <channelpro964@gmail.com>
2024-05-15 18:55:55 +0300

    add

--- cds.py
+++ cds.py
@@ -6,5 +6,5 @@ tokensp = {
     "KEYWORD_KILL": r"KILL \*[0-9][0-9][0-9][0-9]",
     "KEYWORD_ENAP_T": r"ENAP \@1",
     "KEYWORD_ENAP_F": r"ENAP \@0",
-    "KEYWORD_MPTL": r"MPTL \*[0-9][0-9][0-9][0-9] : \\_[a-zA-Z][a-zA-Z][a-zA-Z][a]"
+    "KEYWORD_MPTL": r"MPTL \*[0-9][0-9][0-9][0-9] : \\_[a-zA-Z][a-zA-Z][a-zA-Z][a-Z]"
 }
