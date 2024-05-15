0efdaeda3ecd12e29552b8156fbed8265e99801a
AdamProjectsPrograms <channelpro964@gmail.com>
2024-05-15 18:48:48 +0300

    add

---
+++ gitter.py
@@ -0,0 +1,7 @@
+import os
+import time
+os.system("git config --global user.email \"channelpro964@gmail.com\"")
+os.system("git config --global user.name \"AdamProjectsPrograms\"")
+while True:
+    os.system("git add *")
+    os.system("git commit -m\"add\"")
