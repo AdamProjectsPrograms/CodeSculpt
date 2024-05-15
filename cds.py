03e22acf50540a4376746e38c4d8b076146454c3
AdamProjectsPrograms <channelpro964@gmail.com>
2024-05-15 19:07:40 +0300

    add

--- cds.py
+++ cds.py
@@ -18,7 +18,7 @@ tokensp = {
 TJM_C = 0
 def lexer(code):
     PaT = 0
-    PaTnum = 
+    PaTnum = 0
     for line in open(code, "r").readlines():
         for PAT in tokensp.values():
             if 
